740ece3fc0c7d9efef1589118c33600cb4db7298
tipsy99 <bananaman.paty@gmail.com>
2020-11-23 14:17:24 +0200

    Change irrelevant text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn nbdhjabdkabsd
+                    Learn nbrklp
                 </a>
             </header>
         </div>
